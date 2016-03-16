e7c337c19fdab5d368453fb6337efb38e4608e59
Lucifer-is-my-pet <dianka-piratka@list.ru>
2016-03-16 13:20:39 +0500

    Тестирую всё, что движется а что не движется — двигаю и тестирую

--- test/flow-test.js
+++ test/flow-test.js
@@ -3,59 +3,64 @@ const assert = require('assert');
 const sinon = require('sinon');
 
 describe('Serial', function() {
-    it('should return null on empty argument', function() {
-        var result = flow.serial();
-        assert.equal(result, null);
+    describe('Invalidation check', function() {
+        it('should return null on empty argument', function() {
+            var result = flow.serial();
+            assert.equal(result, null);
+        });
+
+        it('should return null on null argument', function() {
+            var result = flow.serial(null);
+            assert.equal(result, null);
+        });
+
+        it('should return null on not array argument', function() {
+            var result = flow.serial('some string');
+            assert.equal(result, null);
+        });
+
+        it('should return null on wrong array', function() {
+            var result = flow.serial([1, 0]);
+            assert.equal(result, null);
+        });
+
+        it('should return null on empty array', function() {
+            var result = flow.serial([]);
+            assert.equal(result, null);
+        });
     });
 
-    it('should return null on null argument', function() {
-        var result = flow.serial(null);
-        assert.equal(result, null);
+    describe('Functionality check', function() {
+        function testFunc1 (next) {
+            next(null, 100);
+        }
+        function testFunc2 (data, next) {
+            next(new Error(), data);
+        }
+        function testFunc3 (data, next) {
+            next(null, data * 2);
+        }
+        it('should return error', function() {
+            var spy = sinon.spy();
+            flow.serial([testFunc1, testFunc2, testFunc3], spy);
+            assert.equal(spy.calledWith(Error()), true);
+        });
+
+        it('should call functions serially', function() {
+            var spy = sinon.spy();
+            flow.serial([testFunc1, testFunc3], spy);
+            assert.equal(spy.calledWith(null, 200), true);
+        });
     });
 
-    it('should return null on not array argument', function() {
-        var result = flow.serial('some string');
-        assert.equal(result, null);
-    });
-
-    it('should return null on wrong array', function() {
-        var result = flow.serial([1, 0]);
-        assert.equal(result, null);
-    });
-
-    it('should return null on empty array', function() {
-        var result = flow.serial([]);
-        assert.equal(result, null);
-    });
-
-    function testFunc1 (next) {
-        next(null, 100);
-    }
-    function testFunc2 (data, next) {
-        next(new Error(), data);
-    }
-    function testFunc3 (data, next) {
-        next(null, data * 2);
-    }
-    it('should return error', function() {
-        var spy = sinon.spy();
-        flow.serial([testFunc1, testFunc2, testFunc3], spy);
-        assert.equal(spy.calledWith(Error()), true);
-    });
-
-    it('should call functions serially', function() {
-        var spy = sinon.spy();
-        flow.serial([testFunc1, testFunc3], spy);
-        assert.equal(spy.calledWith(null, 200), true);
-    });
 });
 
 describe('Parallel', function() {
-    function testFunc1 () {
-        return 100;
+    function testFunc1 (next) {
+        next(null, 100);
     }
     function testFunc2 (next) {
-        next(new Error());
+        next(new Error(), 234);
     }
     function testFunc3 (next) {
         next(null, 'test text');
@@ -76,7 +81,9 @@ describe('Parallel', function() {
         }
         var fakeTimer = sinon.useFakeTimers();
         flow.parallel([testFunc4, testFunc5]);
-        fakeTimer.tick(2000);
+        fakeTimer.tick(1000);
+        assert.equal(spy.called, false);
+        fakeTimer.tick(1000);
         assert.equal(spy.calledTwice, true);
         fakeTimer.restore();
     });
@@ -84,38 +91,41 @@ describe('Parallel', function() {
 
 describe('Map', function() {
     var testArray = [0, 10];
+    describe('Invalidation check', function() {
+        it('should return null on empty second argument', function() {
+            var result = flow.map(testArray);
+            assert.equal(result, null);
+        });
 
-    it('should return null on empty second argument', function() {
-        var result = flow.map(testArray);
-        assert.equal(result, null);
+        it('should return null on wrong second argument', function() {
+            var result = flow.map(testArray, testArray);
+            assert.equal(result, null);
+        });
+
+        it('should return null on null second argument', function() {
+            var result = flow.map(testArray, null);
+            assert.equal(result, null);
+        });
     });
 
-    it('should return null on wrong second argument', function() {
-        var result = flow.map(testArray, testArray);
-        assert.equal(result, null);
-    });
+    describe('Functionality check', function() {
+        function testFunc1 (data, next) {
+            next(new Error(), data);
+        }
+        it('should return error', function() {
+            var spy = sinon.spy();
+            flow.map(testArray, testFunc1, spy);
+            assert.equal(spy.calledWith(Error()), true);
+        });
 
-    it('should return null on null second argument', function() {
-        var result = flow.map(testArray, null);
-        assert.equal(result, null);
-    });
-
-    function testFunc1 (data, next) {
-        next(null, data + 1);
-    }
-    it('should apply function to array', function() {
-        var spy = sinon.spy();
-        flow.map(testArray, testFunc1, spy);
-        assert.equal(spy.calledWith(null, [1, 11]), true);
-    });
-
-    function testFunc2 (data, next) {
-        next(new Error(), data);
-    }
-    it('should return error', function() {
-        var spy = sinon.spy();
-        flow.map(testArray, testFunc2, spy);
-        assert.equal(spy.calledWith(Error()), true);
+        function testFunc2 (data, next) {
+            next(null, data + 1);
+        }
+        it('should apply function to array', function() {
+            var spy = sinon.spy();
+            flow.map(testArray, testFunc2, spy);
+            assert.equal(spy.calledWith(null, [1, 11]), true);
+        });
     });
 
 });
